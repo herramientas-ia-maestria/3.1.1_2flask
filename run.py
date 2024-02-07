356481c0961225158eb731e6037fc431004454e8
René Elizalde <reroes799@gmail.com>
2024-02-07 18:43:31 -0500

    ejemplo de consumo de un modelo de machine learning con flask

---
+++ run.py
@@ -0,0 +1,27 @@
+from flask import Flask, request, jsonify
+# from sklearn.externals import joblib
+import joblib
+import pandas as pd
+import string
+import scipy
+from nltk.corpus import stopwords
+import pickle
+
+app = Flask(__name__)
+
+@app.route('/predict/<mensaje>', methods=['POST', 'GET'])
+def predict(mensaje=None):
+    """
+    """
+    # mensaje = request.args.get('mensaje')
+    print(mensaje)
+    m = vector.transform([mensaje])
+    prediction = clf.predict(m)
+    return jsonify({'prediction': list(prediction)})
+     # return jsonify({'prediction': mensaje})
+
+if __name__ == '__main__':
+    clf = joblib.load('model01.pkl')
+    # vector = joblib.load('real_vectorizer.pkl', "r")
+    vector = pickle.load(open("vector.pickel", "rb"))
+    app.run(port=8080)
